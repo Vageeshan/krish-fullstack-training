d7c2bca55e325ddb276882c27140e2333740db9d
G.Vageeshan <vageeshanrock@gmail.com>
2022-06-12 12:39:02 +0530

    Home comonent and router related chages added

--- angular/cosmos-ems/src/app/app.module.ts
+++ angular/cosmos-ems/src/app/app.module.ts
@@ -1,24 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { EmployeesComponent } from './employees/employees.component';
-import { LkrFormatterPipe } from './shared/lkr-formatter.pipe';
+import {AppComponent} from './app.component';
+import {EmployeesComponent} from './employees/employees.component';
+import {LkrFormatterPipe} from './shared/lkr-formatter.pipe';
 import {FormsModule} from "@angular/forms";
-import { ProgressBarComponent } from './shared/progress-bar/progress-bar.component';
+import {ProgressBarComponent} from './shared/progress-bar/progress-bar.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
     LkrFormatterPipe,
-    ProgressBarComponent
+    ProgressBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'employees', component: EmployeesComponent},
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
